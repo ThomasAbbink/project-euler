647f660bbdc72e501d2f171ca9b31af5887d6a0e
ThomasAbbink <t.g.abbink@gmail.com>
2025-12-08 13:06:29 +0100

    day 19

---
+++ src/problems/19.ts
@@ -0,0 +1,17 @@
+export function main() {
+  const startYear = 1901;
+  const endYear = 2000;
+  const start = new Date(startYear, 0, 0, 0);
+
+  const endDate = new Date(endYear, 11, 31, 1);
+
+  let count = 0;
+  while (start <= endDate) {
+    if (start.getDay() === 0 && start.getDate() === 1) {
+      console.log(start.toDateString());
+      count++;
+    }
+    start.setDate(start.getDate() + 1);
+  }
+  return count;
+}
