bdde6c60f91cb3cbf01c1f4e2110790ecf2d5e92
ThomasAbbink <t.g.abbink@gmail.com>
2026-01-16 11:38:44 +0100

    30

---
+++ src/problems/30.ts
@@ -0,0 +1,16 @@
+import { sum } from "../utils/array.ts";
+
+export function main() {
+  const size = 5;
+  const res: number[] = [];
+  for (let i = 2; i < 1000000; i++) {
+    const digits = String(i)
+      .split("")
+      .map((d) => Number(d));
+    if (sum(digits.map((d) => Math.pow(d, size))) === i) {
+      res.push(i);
+    }
+  }
+  console.log(res);
+  return sum(res);
+}
