7cb9cc0235bcad6f357eb7aab7af6ad2ea59c626
Lean 4 VS Code Extension <>
2026-01-16 10:19:52 +0100

    Initial commit

---
+++ src/problems/27.ts
@@ -0,0 +1,42 @@
+import { isPrime } from "../utils/primes.ts";
+
+export function main() {
+  const results = [];
+  for (let a = -1000; a < 1000; a++) {
+    for (let b = -1000; b <= 1000; b++) {
+      let n = 0;
+      let hasFoundNonPrime = false;
+      while (!hasFoundNonPrime) {
+        if (isPrime(quadratic(n, a, b))) {
+          n++;
+          continue;
+        }
+
+        if (n > 40) {
+          console.log(`a: ${a}, b:${b} n: ${n} is not prime`);
+          results.push({
+            a,
+            b,
+            n,
+          });
+        }
+        hasFoundNonPrime = true;
+      }
+    }
+  }
+
+  const largestN = results.sort((a, b) => b.n - a.n)[0];
+  return largestN.a * largestN.b;
+}
+
+function quadratic(n: number, a: number, b: number) {
+  return Math.pow(n, 2) + a * n + b;
+}
+
+function eulersquadratic(n: number) {
+  return Math.pow(n, 2) + n + 41;
+}
+
+function incredibleQuadratic(n: number) {
+  return quadratic(n, -79, 1601);
+}
